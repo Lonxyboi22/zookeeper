c120a1a8e02c9ec06be4b2fb83c425db4ad8045a
Lonxyboi <drewthemachine22@gmail.com>
2022-06-29 17:49:36 -0400

    added find by id function

--- server.js
+++ server.js
@@ -52,6 +52,22 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 
+  //filter by id function to narrow it down :)
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
+//looks for a port to use
   app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
