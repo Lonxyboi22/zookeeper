d0cc566160dacb3184e3348645218e884fa03c95
Lonxyboi <drewthemachine22@gmail.com>
2022-07-03 17:10:50 -0400

    handle get and post req

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const { animals } = require('./data/animals.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -65,6 +73,51 @@ app.get('/api/animals', (req, res) => {
     } else {
       res.send(404);
     }
+  });
+
+  //create new animal functions:
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+  
+    //adding animals to the animals.json file
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+      );
+
+    return animal;
+  }
+
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
+  //post for get commands:
+  app.post('/api/animals', (req, res) => {
+        // set id based on what the next index of the array will be
+        req.body.id = animals.length.toString();
+
+          // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+            res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+
   });
 
 //looks for a port to use
